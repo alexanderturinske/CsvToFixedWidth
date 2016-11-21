7faec3c56b467014613d616406f8f7d755bff64e
Alexander James Turinke <a.turinske@gmail.com>
2016-11-20 18:04:02 -0800

    Implement write file function and create output file

--- index.js
+++ index.js
@@ -24,17 +24,7 @@ while (file.length >= i + 5) {
   unprocessedLines.push(line);
   i += 5;
 }
-/*
- *
- * File format: column name, width, pl or pr, filler, data
-lines =
-[
-  [ [ 'one', '1', 'pl', '0', 'B' ],[ 'two', '2', 'pl', '', '1' ], [ 'three', '5', 'pr', '0', '1234' ]],
-  [ [ 'one', '2', 'pl', 'BA' ], [ 'two', '5', 'pl', 'Test' ], [ 'three', '4', 'pr', 'a' ] ]
-]
- * TODO Condense column into a single string
- * Bulk of logic
- */
+
 const processedLines = unprocessedLines.map(function (lineData) {
   return lineData.reduce(function (currentLine, columnData) {
     currentLine = currentLine === void 0 ? '' : currentLine;
@@ -53,27 +43,31 @@ const processedLines = unprocessedLines.map(function (lineData) {
     }
   }, '');
 });
+
+fs.writeFileSync('output.GACT', createFileText(processedLines), 'utf8', function (err) {
+  if (err) throw err;
+  console.log('The file has been created!');
+});
+
 /*
- * TODO Join array of strings into a single string
- * file.map(function (arrayOfStrings) {
- *   return arrayOfStrings.join('');
- * });
- *
- * TODO Write array of lines to a new file
- * fs.writeFileSync('output.GACT', 'utf8');
- *
  * TODO Allow for user specification of input file location and output file location
  *
  */
-function stringConversion(value) {
-  return typeof value === 'string' ? value : value.toString();
+
+function createFileText(data) {
+  return data.join('\n');
 }
+
 function createFiller(filler, data, width) {
   var resultString = '';
   for (let i = 0; i < width - data.length; i++) {
     resultString += filler;
   }
   return resultString;
+}
+
+function stringConversion(value) {
+  return typeof value === 'string' ? value : value.toString();
 }
 
 console.log('pre', unprocessedLines);
